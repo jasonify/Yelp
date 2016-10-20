865133be85ee1f56b1477c5b7543ca655ea1256f
jason <bjason@gmail.com>
2016-10-19 22:23:35 -0700

    a2

---
+++ ios_yelp_swift-master/Yelp/BusinessCell.swift
@@ -0,0 +1,24 @@
+//
+//  BusinessCellTableViewCell.swift
+//  Yelp
+//
+//  Created by jason on 10/19/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
